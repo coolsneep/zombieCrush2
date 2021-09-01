0e45d9321420aac1e2c0bb64f346afdf9a13902c
coolsneep <86134994+coolsneep@users.noreply.github.com>
2021-08-31 21:16:06 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var ground,leftWall,rightWall,link1, bridge, join
+var jointLink,jointPoint,zombie1,background1,zombie,distance
+var stones = []
+var breakButton
+function preload(){
+zombie1 = loadImage("./zombie-crush-assets/assets/zombie.png")
+background1 = loadImage("./zombie-crush-assets/assets/background.png")
+
+}
+function setup() {
+  createCanvas(1800,800);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+  ground = new Base(0, height - 10, width * 2, 20);
+  leftWall = new Base(300, height / 2 + 50, 600, 100);
+  rightWall = new Base(width - 300, height / 2 + 50, 600, 100);
+
+  bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
+  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20);
+
+  Matter.Composite.add(bridge.body, jointPoint);
+
+  jointLink = new Link(bridge, jointPoint);
+
+  for(var i =0;i<=8;i++){
+    var x = random(width/2 -200,width/2+300)
+    var y = random(-10,140)
+    var stone = new Stone(x,y,80,80)
+    stones.push(stone)
+    
+  }
+  console.log("2")
+  zombie = createSprite(width/2-200,height-110,10,10)
+  zombie.addImage("zombie",zombie1)
+  zombie.scale=.1
+  zombie.velocityX=1
+
+  breakButton = createButton("")
+  breakButton.position(width-400,height/20-50)
+  breakButton.size(50,50)
+  breakButton.mousePressed(handleButtonPress);
+}
+
+function draw() {
+  console.log("3")
+  background(background1);
+  Engine.update(engine);
+leftWall.display()
+rightWall.display()
+ground.display()
+bridge.show()
+
+
+
+for(var stone of stones){
+stone.display()
+var pos = stone.body.position
+distance = dist(zombie.position.x,zombie.position.y,pos.x,pos.y)
+if (distance<=50){
+  zombie.velocityX=0
+  Body.setVelocity(stone.body,{x:10,y:-10})
+}
+}
+drawSprites()
+}
+
+function handleButtonPress(){
+  jointLink.detach();
+  setTimeout(() => {
+    bridge.break();
+  }, 1500);
+}
